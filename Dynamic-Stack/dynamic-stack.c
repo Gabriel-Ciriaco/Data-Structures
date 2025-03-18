b1973ce11327f913cf07348ea6165fc71d789590
Gasegamer <gabrielciricarvalho@gmail.com>
2025-03-18 17:45:13 -0300

    Implemented base functions to Dynamic Stack!

--- Dynamic-Stack/dynamic-stack.c
+++ Dynamic-Stack/dynamic-stack.c
@@ -1,1 +1,58 @@
+#include "dynamic-stack.h"
+
+#include <stdlib.h>
+
+
+Node * createNode(int value)
+{
+    Node * newNode = (Node *) malloc(sizeof(Node));
+
+    newNode->value = value;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void createDynamicStack()
+{
+    DynamicStack newStack;
+
+    newStack->top = NULL;
+
+    return newStack;
+}
+
+void push(int value, DynamicStack * dStack)
+{
+    Node * newValue = createNode(value);
+
+    if (!dStack->top)
+    {
+        dStack->top = newValue;
+    }
+    else
+    {
+        Node * cur = dStack->top;
+
+        newValue->next = cur;
+
+        dStack->top = newValue;
+    }
+}
+
+void pop(DynamicStack * dStack)
+{
+    if (!dStack->top)
+    {
+        printf("\nThe Dynamic Stack is empty!\n");
+    }
+    else
+    {
+        Node * cur = dStack->top;
+
+        dStack->top = cur->next;
+
+        free(cur);
+    }
+}
 
--- Dynamic-Stack/dynamic-stack.h
+++ Dynamic-Stack/dynamic-stack.h
@@ -1,6 +1,7 @@
 #ifndef DYNAMIC-STACK_H
 #define DYNAMIC-STACK_H
 
+
 typedef struct node
 {
     int value;
@@ -17,8 +18,10 @@ Node * createNode(int value);
 
 void createDynamicStack();
 
-void push(int value);
+void push(int value, DynamicStack * dStack);
 
-int pop();
+int pop(DynamicStack * dStack);
+
+void cleanStack(DynamicStack * dStack);
 
 #endif // DYNAMIC-STACK_H
